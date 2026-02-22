549c634bf36ab05547c9a69592fc6176d6d8adae
SoriPhoono <96075107+soriPhoono@users.noreply.github.com>
2026-02-22 08:32:14 -0600

    Delete antigravity-build.nix

--- tests/antigravity-build.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}:
-if pkgs.stdenv.hostPlatform.system == "x86_64-linux"
-then pkgs.antigravity
-else
-  pkgs.runCommand "antigravity-skip" {} ''
-    echo "Skipping Antigravity test on ${pkgs.stdenv.hostPlatform.system}"
-    touch $out
-  ''
