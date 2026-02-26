9fea3e67383c0d1bece99e62ab59c5c40b781fab
soriphoono <soriphoono@gmail.com>
2026-02-26 13:06:14 -0600

    chore: organized option names and cleaned up basic docker runtime

--- modules/nixos/hosting/blocks/backends/default.nix
+++ modules/nixos/hosting/blocks/backends/default.nix
@@ -1,6 +1,21 @@
 {
-  imports = [
-    ./docker.nix
-    ./podman.nix
-  ];
-}
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.hosting.blocks.backends;
+in
+  with lib; {
+    imports = [
+      ./docker.nix
+      ./podman.nix
+    ];
+
+    options.hosting.blocks.backends = {
+      enableNvidiaSupport = mkEnableOption "Enable nvidia support for blocks";
+    };
+
+    config = mkIf cfg.enable {
+      hardware.nvidia-container-toolkit.enable = mkIf config.core.hardware.gpu.dedicated.nvidia.enable true;
+    };
+  }
--- modules/nixos/hosting/default.nix
+++ modules/nixos/hosting/default.nix
@@ -1,18 +1,5 @@
 {
-  lib,
-  config,
-  ...
-}:
-with lib; {
   imports = [
     ./blocks
   ];
-
-  options.hosting = {
-    enable = mkEnableOption "Enable hosting features";
-  };
-
-  config = mkIf config.hosting.enable {
-    hardware.nvidia-container-toolkit.enable = mkIf config.core.hardware.gpu.dedicated.nvidia.enable true;
-  };
 }
