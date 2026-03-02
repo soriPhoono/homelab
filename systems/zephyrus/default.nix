2c1f86de0fc2b87a2dc6c3e8ada9a57829ada388
soriphoono <soriphoono@gmail.com>
2026-03-02 14:39:05 -0500

    chore: updated flake, added hosting module back to zephyrus for testbench

--- systems/zephyrus/default.nix
+++ systems/zephyrus/default.nix
@@ -54,6 +54,9 @@
         hashedPassword = "$6$x7n.SUTMtInzs2l4$Ew3Zu3Mkc4zvuH8STaVpwIv59UX9rmUV7I7bmWyTRjomM7QRn0Jt/Pl/JN./IqTrXqEe8nIYB43m1nLI2Un211";
         admin = true;
         shell = pkgs.fish;
+        extraGroups = [
+          "docker"
+        ];
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgxxFcqHVwYhY0TjbsqByOYpmWXqzlVyGzpKjqS8mO7";
         subUidRanges = [
           {
@@ -85,4 +88,6 @@
     };
     services.asusd.enable = true;
   };
+
+  hosting.single-node.backends.docker.enable = true;
 }
