002e6f5ed7a44c9787a144154cc6e1cea1053458
soriphoono <soriphoono@gmail.com>
2026-04-09 22:44:21 -0500

    Add comprehensive Zed editor extensions for multi-language and DevOps development

--- nix/homes/sphoono/configs/zed/extensions.nix
+++ nix/homes/sphoono/configs/zed/extensions.nix
@@ -1,6 +1,47 @@
 {
   userapps.development.editors.zed.extensions = [
+    # Desktop / System
+    "dockerfile" # Dockerfile syntax highlighting and LSP
+    "docker-compose" # Docker Compose YAML support
+
+    # Web2 Development
+    "html" # HTML support
+    "deno" # Deno runtime (JS/TS server-side)
+    "astro" # Astro web framework
+    "vue" # Vue.js framework
+    "svelte" # Svelte framework
+    "tailwindcss" # Tailwind CSS framework
+
+    # Web3 Development
+    "tailwindcss" # Used in Web3 dapps too
+
+    # DevOps
+    "terraform" # Terraform IaC syntax
+    "ansible" # Ansible playbooks
+    "helm" # Helm charts (K8s package manager)
+    "kubernetes" # Kubernetes manifests
+
+    # Firmware
+    "arduino" # Arduino firmware development
+
+    # Languages
     "nix"
     "bash"
+    "lua"
+    "perl"
+    "python"
+    "css"
+    "scss"
+    "javascript"
+    "typescript"
+    "golang"
+    "rust"
+    "java"
+    "csharp"
+    "zig"
+    "ocaml"
+
+    # Extra useful
+    "git-firefly" # Enhanced Git integration (commits, branches, etc.)
   ];
 }
