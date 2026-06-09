bfa893ad2e7f60f815a2c82f2f62268af5ac998e
SoriPhoono <soriphoono@gmail.com>
2026-06-09 12:34:47 -0400

    Cleaned up ares configuration

--- nix/homes/sphoono@ares/userapps.nix
+++ nix/homes/sphoono@ares/userapps.nix
@@ -22,30 +22,32 @@ _: {
       };
       office = {
         zathura.enable = true;
+        calibre.enable = true;
         libreoffice.enable = true;
         slack.enable = true;
       };
       virtualization = {
-        distrobox.enable = true;
+        # distrobox.enable = true;
         # bottles.enable = true;
       };
     };
     development = {
       enable = true;
       terminal.ghostty.enable = true;
+      agents.pi.enable = true;
       editors.zed.enable = true;
-      appliances.bambu-studio.enable = true;
+      # appliances.bambu-studio.enable = true;
     };
     data-fortress = {
-      cloud.nextcloud.enable = true;
-      auth.bitwarden.enable = true;
+      # cloud.nextcloud.enable = true;
+      # auth.bitwarden.enable = true;
       notes.obsidian.enable = true;
       p2p.qbittorrent.enable = true;
     };
     content-creation = {
       asset-creation = {
         gimp.enable = true;
-        blender.enable = true;
+        # blender.enable = true;
       };
       streaming.obs-studio.enable = true;
       editors = {
