85de6f729c48d87871c68c9bd0f69391e756480a
soriphoono <soriphoono@gmail.com>
2026-08-01 16:34:25 -0500

    Migrated hermes skills to general agent skills for antigravity/opencode compatibility, re-introduced nodejs, pnpm, and python into home environment for agentic tools and nodejs

--- nix/modules/home/apps/development/default.nix
+++ nix/modules/home/apps/development/default.nix
@@ -9,4 +9,15 @@ with lib; {
     ./infrastructure
     ./terminal
   ];
+
+  config = {
+    home.packages = with pkgs; [
+      (python3.withPackages (python-pkgs:
+        with python-pkgs; [
+          # TODO: build out global python environment for agentic development
+        ]))
+      nodejs
+      pnpm
+    ];
+  };
 }
